46020c753967535b69e22ecef883c71259475864
JuanCamiloMartinezLopez <juacmartinezl@correo.udistrital.edu.co>
2024-01-12 03:46:39 -0500

    feature: se ajusta la función de bootstraping de single-spa y se cambia la etiqueta principal

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,15 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { APP_BASE_HREF } from '@angular/common';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { getSingleSpaExtraProviders } from 'single-spa-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    { provide: APP_BASE_HREF, useValue: '/' },
+    getSingleSpaExtraProviders(),
+    provideHttpClient(withFetch())
+  ]
 };
--- src/main.single-spa.ts
+++ src/main.single-spa.ts
@@ -1,14 +1,14 @@
 import { enableProdMode, NgZone } from '@angular/core';
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { Router, NavigationStart } from '@angular/router';
 
-import { singleSpaAngular, getSingleSpaExtraProviders } from 'single-spa-angular';
+import { singleSpaAngular } from 'single-spa-angular';
 
-
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { singleSpaPropsSubject } from './single-spa/single-spa-props';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
 if (environment.production) {
   enableProdMode();
@@ -17,9 +17,9 @@ if (environment.production) {
 const lifecycles = singleSpaAngular({
   bootstrapFunction: singleSpaProps => {
     singleSpaPropsSubject.next(singleSpaProps);
-    return platformBrowserDynamic(getSingleSpaExtraProviders()).bootstrapModule(AppModule);
+    return bootstrapApplication(AppComponent, appConfig);
   },
-  template: '<app-root />',
+  template: '<sga-datos-mf />',
   Router,
   NavigationStart,
   NgZone,
