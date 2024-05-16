e7286a6b44d9ca0cfba3ea0373d36de2332d19e5
bfmunozm <swsbmm@gmail.com>
2024-05-16 10:11:38 -0500

    feat: Ajuste nombre de rutas  udistrital/sga_documentacion#196

--- src/app/solicitudes/solicitudes-routing.module.ts
+++ src/app/solicitudes/solicitudes-routing.module.ts
@@ -1,39 +1,34 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { SolicitudesComponent } from "./solicitudes.component";
-import { ListSolicitudesEstudianteComponent } from "./list-solicitudes-estudiante/list-solicitudes-estudiante.component";
-import { AuthGuard } from "../_guards/auth.guard";
-import { ActualizacionDatosComponent } from "./actualizacion-datos/actualizacion-datos.component";
-import { DatosSolicitanteComponent } from "./datos-solicitante/datos-solicitante.component";
-import { ViewSolicitudesComponent } from "./view-solicitudes/view-solicitudes.component";
-import { ActualizacionNombresComponent } from "./actualizacion-nombres/actualizacion-nombres.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SolicitudesComponent } from './solicitudes.component';
+import { ListSolicitudesEstudianteComponent } from './list-solicitudes-estudiante/list-solicitudes-estudiante.component';
+import { ActualizacionDatosComponent } from './actualizacion-datos/actualizacion-datos.component';
+import { DatosSolicitanteComponent } from './datos-solicitante/datos-solicitante.component';
+import { ViewSolicitudesComponent } from './view-solicitudes/view-solicitudes.component';
+import { ActualizacionNombresComponent } from './actualizacion-nombres/actualizacion-nombres.component';
 
-const routes: Routes = [{
-    path: '',
+const routes: Routes = [
+  {
+    path: 'datos-basicos',
     component: ListSolicitudesEstudianteComponent,
-    children: [
-        {
-            path: 'actualizacion-datos',
-            component: ViewSolicitudesComponent,
-        }
-    ],
-}];
+  },
+  {
+    path: 'actualizacion-datos',
+    component: ViewSolicitudesComponent,
+  },
+];
 
 @NgModule({
-    imports: [
-        RouterModule.forChild(routes),
-    ],
-    exports: [
-        RouterModule,
-    ],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class SolicitudesRoutingComponent { }
+export class SolicitudesRoutingComponent {}
 
 export const routedComponents = [
-    SolicitudesComponent,
-    ListSolicitudesEstudianteComponent,
-    ActualizacionDatosComponent,
-    DatosSolicitanteComponent,
-    ViewSolicitudesComponent,
-    ActualizacionNombresComponent
-]
+  SolicitudesComponent,
+  ListSolicitudesEstudianteComponent,
+  ActualizacionDatosComponent,
+  DatosSolicitanteComponent,
+  ViewSolicitudesComponent,
+  ActualizacionNombresComponent,
+];
