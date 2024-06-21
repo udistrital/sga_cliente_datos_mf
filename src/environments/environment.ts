ce163ac5b66bc6effe6573df550e55ea29371da2
bfmunozm <swsbmm@gmail.com>
2024-06-21 13:36:46 -0500

    feat: Actualizar URL de servicios en environment.ts
    Se actualizó la URL del servicio NUXEO_SERVICE en el archivo environment.ts para apuntar a la nueva dirección de pruebasapi2.intranetoas.udistrital.edu.co. También se agregaron las URLs de los servicios TERCEROS_MID_SERVICE y DOCUMENTO_SERVICE para su uso en otras partes del sistema.
    
    udistrital/sga_documentacion#287

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,9 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:4202/',
-    NUXEO_SERVICE:'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1',
+    NUXEO_SERVICE:'http://pruebasapi2.intranetoas.udistrital.edu.co:8199/v1/',
     TERCEROS_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8121/v1/',
-    SGA_MID_ACTUALIZACION_DATOS_SERVICE: 'http://pruebasapi3.intranetoas.udistrital.edu.co:8548/v1/'
+    TERCEROS_MID_SERVICE: 'http://pruebasapi2.intranetoas.udistrital.edu.co:8123/v1/',
+    SGA_MID_ACTUALIZACION_DATOS_SERVICE: 'http://pruebasapi3.intranetoas.udistrital.edu.co:8548/v1/',
+    DOCUMENTO_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8094/v1/'
 };
