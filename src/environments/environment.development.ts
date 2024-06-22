58c3afe82d6b7d631eb86e6e12d78ff26270fe9c
Bryan Muñoz <swsbmm@gmail.com>
2024-06-22 10:43:39 -0500

    feat: Actualizar URL de servicios en environment.ts  udistrital/sga_documentacion#287

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: false,
   apiUrl: 'https://pruebassgadatos.portaloas.udistrital.edu.co/',
-  SGA_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/sga_mid/v1/',
-  NUXEO_SERVICE:'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1',
+  NUXEO_SERVICE:'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1/',
   TERCEROS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/terceros_crud/v1/',
-  SGA_MID_ACTUALIZACION_DATOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/admisiones_mid/v1/'
+  TERCEROS_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/programa_academico_crud/v1/',
+  SGA_MID_ACTUALIZACION_DATOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/admisiones_mid/v1/',
+  DOCUMENTO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/documento_crud/v2/'
 };
