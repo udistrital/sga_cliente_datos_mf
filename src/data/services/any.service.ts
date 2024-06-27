ff807c7905b3da558e7570ab83d153f5eb8dfdd4
bfmunozm <swsbmm@gmail.com>
2024-06-27 09:53:50 -0500

    feat: Actualizar httpOptions en AnyService
    Se actualizó el código en el archivo `any.service.ts` para utilizar la propiedad `httpOptions` en lugar de la constante `httpOptions` en los métodos `get`, `getp`, `post`, `put`, `put2`, `delete` y `delete2`. Esto permite que los encabezados de la solicitud HTTP se configuren correctamente y mejora la seguridad y el rendimiento del servicio.
    
    udistrital/sga_documentacion#287

--- src/data/services/any.service.ts
+++ src/data/services/any.service.ts
@@ -4,77 +4,91 @@ import { throwError } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 
-const httpOptions = {
-    headers: new HttpHeaders({
-        'Accept': 'application/json',
-    }),
-}
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class AnyService {
+  private httpOptions: any;
 
-    constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {
+    const access_token = window.localStorage.getItem('access_token');
+    if (access_token) {
+      this.httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${access_token}`,
+        }),
+      };
+    } else {
+      this.httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+        }),
+      };
     }
-
-    get(path, endpoint) {
-        return this.http.get(path + endpoint, httpOptions).pipe(
-          catchError(this.handleError),
-        );
-    }
-
-    getp(path, endpoint) {
-      return this.http.get(path + endpoint, { ...httpOptions, reportProgress: true, observe: 'events' }).pipe(
-        catchError(this.handleError),
-      );
   }
 
-    post(path, endpoint, element) {
-        return this.http.post(path + endpoint, element, httpOptions).pipe(
-          catchError(this.handleError),
-        );
-    }
-
-    put(path, endpoint, element) {
-        return this.http.put(path + endpoint + '/', element, httpOptions).pipe(
-          catchError(this.handleError),
-        ); // + element.Id
-    }
-    put2(path, endpoint, element) {
-      return this.http.put(path + endpoint + '/' + element.Id, element, httpOptions).pipe(
-        catchError(this.handleError),
-      );
-    }
-
-    delete(path, endpoint, element) {
-        return this.http.delete(path + endpoint + '/' + element.Id, httpOptions).pipe(
-          catchError(this.handleError),
-        );
-    }
-
-    delete2(path, endpoint) {
-      return this.http.delete(path + endpoint, httpOptions).pipe(
-        catchError(this.handleError),
-      );
+  get(path, endpoint) {
+    return this.http
+      .get(path + endpoint, this.httpOptions)
+      .pipe(catchError(this.handleError));
   }
 
-    private handleError(error: HttpErrorResponse) {
-        if (error.error instanceof ErrorEvent) {
-          // A client-side or network error occurred. Handle it accordingly.
-          console.error('An error occurred:', error.error.message);
-        } else {
-          // The backend returned an unsuccessful response code.
-          // The response body may contain clues as to what went wrong,
-          console.error(
-            `Backend returned code ${error.status}, ` +
-            `body was: ${error.error}`);
-        }
-        // return an observable with a user-facing error message
-        return throwError({
-          status: error.status,
-          message: 'Something bad happened; please try again later.',
-        });
-    };
+  getp(path, endpoint) {
+    return this.http
+      .get(path + endpoint, {
+        ...this.httpOptions,
+        reportProgress: true,
+        observe: 'events',
+      })
+      .pipe(catchError(this.handleError));
+  }
+
+  post(path, endpoint, element) {
+    return this.http
+      .post(path + endpoint, element, this.httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+  put(path, endpoint, element) {
+    return this.http
+      .put(path + endpoint + '/', element, this.httpOptions)
+      .pipe(catchError(this.handleError)); // + element.Id
+  }
+  put2(path, endpoint, element) {
+    return this.http
+      .put(path + endpoint + '/' + element.Id, element, this.httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+  delete(path, endpoint, element) {
+    return this.http
+      .delete(path + endpoint + '/' + element.Id, this.httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+  delete2(path, endpoint) {
+    return this.http
+      .delete(path + endpoint, this.httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    // return an observable with a user-facing error message
+    return throwError({
+      status: error.status,
+      message: 'Something bad happened; please try again later.',
+    });
+  }
 }
